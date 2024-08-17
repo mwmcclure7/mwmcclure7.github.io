c6fe405046a719f032660fe40e3c9e040a44a76d
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2024-08-17 17:42:01 -0400

    Added password protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -13,26 +15,43 @@ import Robotics from "./pages/Robotics";
 import Programming from "./pages/Programming";
 import Printing from "./pages/Printing";
 import Volunteering from "./pages/Volunteering";
+import Login from "./pages/Login";
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    function handleLogin() {
+        setIsAuthenticated(true);
+    }
+
     return (
         <div className="app">
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/academics" element={<Academics />} />
-                <Route path="/interests" element={<Interests />} />
-                <Route path="/edge" element={<Edge />} />
-                <Route path="/sensei" element={<Sensei />} />
-                <Route path="/karate" element={<Karate />} />
-                <Route path="/mtb" element={<Mtb />} />
-                <Route path="/aerospace" element={<Aerospace />} />
-                <Route path="/robotics" element={<Robotics />} />
-                <Route path="/programming" element={<Programming />} />
-                <Route path="/printing" element={<Printing />} />
-                <Route path="/volunteering" element={<Volunteering />} />
-            </Routes>
-            <Footer />
+            {isAuthenticated ? (
+                <div>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/academics" element={<Academics />} />
+                        <Route path="/interests" element={<Interests />} />
+                        <Route path="/edge" element={<Edge />} />
+                        <Route path="/sensei" element={<Sensei />} />
+                        <Route path="/karate" element={<Karate />} />
+                        <Route path="/mtb" element={<Mtb />} />
+                        <Route path="/aerospace" element={<Aerospace />} />
+                        <Route path="/robotics" element={<Robotics />} />
+                        <Route path="/programming" element={<Programming />} />
+                        <Route path="/printing" element={<Printing />} />
+                        <Route
+                            path="/volunteering"
+                            element={<Volunteering />}
+                        />
+                    </Routes>
+                    <Footer />
+                </div>
+            ) : (
+                <Login onLogin={handleLogin} />
+            )}
+            <Toaster />
         </div>
     );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,31 @@
+import toast from "react-hot-toast";
+import "../styles/Login.css";
+
+interface LoginProps {
+    onLogin: () => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        toast.error("Incorrect password");
+    };
+
+    function checkPassword(password: string) {
+        if (password === import.meta.env.VITE_PASSWORD) onLogin();
+    }
+
+    return (
+        <div className="login">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="password"
+                    onChange={(e) => checkPassword(e.target.value)}
+                    placeholder="Enter password"
+                />
+            </form>
+        </div>
+    );
+};
+
+export default Login;
